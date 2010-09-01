dd8f4686b1c051da65a3364e70c9bdf0662659b0
Kristof Rennen <contact@kristofrennen.be>
2010-09-01 17:36:47 +0200

    Documenting code and Sandcastle project

--- Agility.Common/Infrastructure/ComponentProvider.cs
+++ Agility.Common/Infrastructure/ComponentProvider.cs
@@ -1,9 +1,11 @@
-using System;
-using System.Linq;
 using StructureMap;
 
 namespace Agility.Common.Infrastructure
 {
+    /// <summary>
+    /// Wrapper around an Inversion of Control container, managing all components and its dependencies.
+    /// The combination of contract and implementation should be unique.
+    /// </summary>
     public static class ComponentProvider
     {
         private static IContainer container;
@@ -13,11 +15,21 @@ namespace Agility.Common.Infrastructure
             ReBuild();
         }
 
+        /// <summary>
+        /// Releases all registered components and rebuilds the container.
+        /// </summary>
         public static void ReBuild()
         {
             container = new Container(c => { });
         }
 
+        /// <summary>
+        /// Registers a transient component within the container if the given contract / implementation combination was not registered yet.
+        /// Throws a ComponentRegistrationException when a component was already registered for the given contract. 
+        /// </summary>
+        /// <exception cref="Agility.Common.Infrastructure.ComponentRegistrationException">When an implementation was already registered for the given contract.</exception>
+        /// <typeparam name="TInterface">The contract of the component to register.</typeparam>
+        /// <typeparam name="TImplementation">The implementation of the component to register.</typeparam>
         public static void Register<TInterface, TImplementation>() where TImplementation : TInterface
         {
             CheckComponentUniqueness<TInterface>();
@@ -25,6 +37,13 @@ namespace Agility.Common.Infrastructure
             container.Configure(o => o.For<TInterface>().Use<TImplementation>());
         }
 
+        /// <summary>
+        /// Registers a singleton component within the container if the given contract / implementation combination was not registered yet.
+        /// Throws a ComponentRegistrationException when a component was already registered for the given contract. 
+        /// </summary>
+        /// <exception cref="Agility.Common.Infrastructure.ComponentRegistrationException">When an implementation was already registered for the given contract.</exception>
+        /// <typeparam name="TInterface">The contract of the component to register.</typeparam>
+        /// <typeparam name="TImplementation">The implementation of the component to register.</typeparam>
         public static void RegisterSingleton<TInterface, TImplementation>() where TImplementation : TInterface
         {
             CheckComponentUniqueness<TInterface>();
@@ -32,11 +51,23 @@ namespace Agility.Common.Infrastructure
             container.Configure(o => o.For<TInterface>().Singleton().Use<TImplementation>());
         }
 
+        /// <summary>
+        /// Returns whether a component for a given contract was registered within the container.
+        /// </summary>
+        /// <typeparam name="TInterface">The contract of the component to check.</typeparam>
+        /// <returns>True if the component was registered, False when no component was registered.</returns>
         public static bool HasRegisteredComponentFor<TInterface>()
         {
             return container.Model.HasImplementationsFor<TInterface>();
         }
 
+        /// <summary>
+        /// Returns the implementation for a given contract from the container.
+        /// Throws a ComponentRegistrationException when no component was registered for the given contract. 
+        /// </summary>
+        /// <exception cref="Agility.Common.Infrastructure.ComponentRegistrationException">When an implementation is not registered for the given contract.</exception>
+        /// <typeparam name="TInterface">The contract of the component to resolve.</typeparam>
+        /// <returns>The implementation for the given contract from the container.</returns>
         public static TInterface Resolve<TInterface>()
         {
             if (!HasRegisteredComponentFor<TInterface>())
@@ -47,6 +78,13 @@ namespace Agility.Common.Infrastructure
             return container.GetInstance<TInterface>();
         }
 
+        /// <summary>
+        /// Checks whether an implementation for the given contract was already registered within the container.
+        /// Does nothing when no component was registered yet.
+        /// Throws a ComponentRegistrationException when a component was already registered for the given contract. 
+        /// </summary>
+        /// <exception cref="Agility.Common.Infrastructure.ComponentRegistrationException">When an implementation is registered for the given contract.</exception>
+        /// <typeparam name="TInterface">The contract of the component to check.</typeparam>
         private static void CheckComponentUniqueness<TInterface>()
         {
             if (HasRegisteredComponentFor<TInterface>())
