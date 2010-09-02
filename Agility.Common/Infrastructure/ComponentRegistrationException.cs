6824195259863afa0641e20511980b577a1dad91
Kristof Rennen <contact@kristofrennen.be>
2010-09-02 20:13:36 +0200

    Adding comments for ComponentRegistrationException

--- Agility.Common/Infrastructure/ComponentRegistrationException.cs
+++ Agility.Common/Infrastructure/ComponentRegistrationException.cs
@@ -2,8 +2,15 @@ using System;
 
 namespace Agility.Common.Infrastructure
 {
+    /// <summary>
+    /// The exception which is thrown when component registration or resolution fails.
+    /// </summary>
     public class ComponentRegistrationException : ApplicationException
     {
+        /// <summary>
+        /// Initializes a new ComponentRegistrationException with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message.</param>
         public ComponentRegistrationException(string message) : base(message) { }
     }
 }
