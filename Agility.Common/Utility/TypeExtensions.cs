b047ff5443ac3ee5f390687eb7731be792e40c60
Kristof Rennen <contact@kristofrennen.be>
2010-09-14 15:17:50 +0200

    Updating code documentation

--- Agility.Common/Utility/TypeExtensions.cs
+++ Agility.Common/Utility/TypeExtensions.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace Agility.Common.Utility
 {
+    /// <summary>
+    /// Custom behaviour for <see cref="System.Type" />
+    /// </summary>
     public static class TypeExtensions
     {
+        /// <summary>
+        /// Returns a nice name for the given type in the format Namespace.Class&lt;Namespace.Class&gt;.
+        /// </summary>
+        /// <param name="type">The type to get the nice name for.</param>
+        /// <returns>The nice name for the given type.</returns>
         public static string NiceName(this Type type)
         {
             if (type == null)
